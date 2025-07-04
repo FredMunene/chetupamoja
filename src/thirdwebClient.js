1e84469944c48ea7ff9f5c51a96297aeda6af2f9
fgitonga <mfredgitonga@gmail.com>
2025-07-04 20:04:53 +0300

    Refactor thirdweb client integration to use ThirdwebSDK
    - Replaced the deprecated createThirdwebClient with ThirdwebSDK for improved functionality.
    - Updated client configuration to utilize dynamic clientId and specified chain and RPC URL for Lisk network.

--- src/thirdwebClient.js
+++ src/thirdwebClient.js
@@ -1,14 +1,15 @@
-import { createThirdwebClient } from "thirdweb";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
 
-console.log('Thirdweb clientId:', clientId); // Debug: print the clientId
 
 const secretKey = import.meta.env.VITE_TEMPLATE_SECRET_KEY;
 
-export const client = createThirdwebClient({
-    clientId: "D3kphw_r2J7IXeoIp6j8TH0sDm7IaoUUMu0xF4i_Tjeb4j9v193BivWqBQMsPcVeLgg6IZfEFW3LEhWLdOBiYw",
-    secretKey: secretKey,
-  });
+
+export const client = new ThirdwebSDK({
+  chain: 1135,
+  rpcUrl: "https://rpc.api.lisk.com",
+  clientId: clientId,
+});
