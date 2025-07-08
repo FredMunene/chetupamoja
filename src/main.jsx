6e06c4dbec28d02d65fbaa8c65fd6c09cc4b0991
FredMunene <munene2050gitonga@gmail.com>
2025-07-08 16:00:29 +0300

    Integrate Wallet and Wagmi providers into the main application

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,32 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App.jsx';
 import './index.css';
+import { WalletProvider } from './contexts/WalletContext';
+// import "./polyfills";
 
+import { WagmiConfig, chain, createClient } from "wagmi";
+import { ConnectKitProvider, getDefaultClient } from "connectkit";
 
-const clientId = import.meta.env.VITE_TEMPLATE_CLIENT_ID;
-createRoot(document.getElementById('root')).render(
+const client = createClient(
+  getDefaultClient({
+    appName: "ConnectKit Vite Demo",
+    alchemyId: import.meta.env.VITE_ALCHEMY_ID, // or process.env if not using Vite
+    // infuraId: import.meta.env.VITE_INFURA_ID,
+    chains: [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  })
+);
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
   <StrictMode>
-
-      <App />
-
+    <WagmiConfig client={client}>
+      <ConnectKitProvider theme="auto">
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </ConnectKitProvider>
+    </WagmiConfig>
   </StrictMode>
 );
